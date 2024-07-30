a51214d8d76e5d4b761023eec4d89f2429802ff6
ota-shawn <shawn.b@osakititan.com>
2024-07-30 16:48:12 -0500

    damn

--- app/api/here/route.ts
+++ app/api/here/route.ts
@@ -1,6 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = (req: NextRequest, res: NextResponse) => {
-  console.log(req.headers);
-  return NextResponse.json({ message: JSON.stringify(req.headers) });
+export const GET = (req: NextRequest) => {
+  // Convert headers to a plain object
+  const headersObj = Object.fromEntries(req.headers);
+
+  // Log the headers object
+  console.log(JSON.stringify(headersObj, null, 2));
+
+  return NextResponse.json({ headers: headersObj });
 };
